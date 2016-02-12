1a95bb43e9add941b68361f9a387a55b133c1d9b
Brandon Gottschling <Brandon Gottschling>
2016-02-12 01:55:34 -0500

    edited scripts

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@
 function getWeather(lat, lon) {
 	
   var apiURI = "https://api.openweathermap.org/data/2.5/weather?lat="+ lat +"&lon="+ lon +"&appid=06170c100199dbae1e223cc3dfad960b";
-  var obj;
+  /*var obj;
   var xhr = new XMLHttpRequest();
 
   xhr.open("GET", apiURI, true);
@@ -13,9 +13,9 @@ function getWeather(lat, lon) {
 
   obj = xhr.response;
 
-  console.log(obj);
+  console.log(obj);*/
 
- /* $.ajax({
+  $.ajax({
   	url: apiURI,
     type: "GET",
     success: function(resp) {
@@ -26,7 +26,7 @@ function getWeather(lat, lon) {
       console.log("an error has occurred")
     	alert(error);
     }
-  });*/
+  });
  }
 
 if ("geolocation" in navigator) {
