fb9b69d0a2da771c611aa137b7d46e75769ec205
Brandon Gottschling <Brandon Gottschling>
2016-02-12 02:04:49 -0500

    edited scripts

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@
 
 function getWeather(lat, lon) {
 	
-  var apiURI = "https://api.openweathermap.org/data/2.5/weather?lat="+ lat +"&lon="+ lon +"&appid=06170c100199dbae1e223cc3dfad960b";
+  var apiURI = "http://api.openweathermap.org/data/2.5/weather?lat="+ lat +"&lon="+ lon +"&appid=06170c100199dbae1e223cc3dfad960b";
   /*var obj;
   var xhr = new XMLHttpRequest();
 
