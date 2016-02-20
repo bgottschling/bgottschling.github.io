59a7d6050caf330a7df8c985d21e933c6c08fdf1
Brandon Gottschling <Brandon Gottschling>
2016-02-20 00:14:18 -0500

    updated HTML, CSS & refactored the script for encapsulation, and handle data from an asynchronous request more efficiently.

--- js/weather.js
+++ js/weather.js
@@ -1,78 +1,78 @@
-var tempMode = 1;
+//necessary global variables for selector caching.
+var $tempMode = $("#tempMode");
+var $tempText = $("#temp-text");
+var $windText = $("#wind-text");
+$(document).ready(function() {
+  // this function takes the temperature from the dataHandler and displays the temperature according to the correct temperature unit, and colors the temperature hot or cold.
 
+  function formatTemperature(kelvin) {
+    var clicked = false;
+    var fahr = ((kelvin * 9 / 5) - 459.67).toFixed(0);
+    var cels = (kelvin - 273.15).toFixed(1);
+    //initial temperature display
+    $tempText.html(fahr);
+    //click handler to update the temperature unit of measurement.
+    $tempMode.off("click").on("click", function() {
+      console.log(clicked);
+      clicked === false ? clicked = true : clicked = false;
+      clicked === true ? $tempMode.html("C&deg") : $tempMode.html("F&deg");
+      if (clicked) {
+        $tempText.html(cels);
+      } else
+        $tempText.html(fahr);
+    });
 
-
-function getWeather(lat, lon) {
-  var apiURI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=06170c100199dbae1e223cc3dfad960b";
-
-  $.ajax({
-    url: apiURI,
-    dataType: "json",
-    type: "GET",
-    async: "false",
-    success: function(resp) {
-
-      $("#tempMode").on("click", function() {
-        if (this.checked) {
-          $("#temp-text").html(cels.toFixed(1) + " C&deg");
-          console.log("checked");
-        } else
-          $("#temp-text").html(fahr.toFixed(0) + " F&deg");
-      });
-      console.log(apiURI);
-      console.log(resp.name);
-      if (resp.name) {
-        $("#city-text").html(resp.name + ", " + resp.sys.country);
-      }
-      if (resp.wind) {
-        var knots = resp.wind.speed * 1.9438445;
-        $("#wind-text").html(knots.toFixed(1) + " Knots");
-      }
-      if (resp.main.temp) {
-        var fahr = (resp.main.temp * 9 / 5) - 459.67;
-        var cels = (resp.main.temp - 273.15);
-        if (cels > 24){ 
-          $("#temp-text").css("color", "red");
-        } else if (cels < 18){
-          $("#temp-text").css("color", "blue");
-        }
-        $("#temp-text").html((tempMode === 1 ? fahr.toFixed(0) + " F&deg" : cels.toFixed(0) + " C&deg"));
-      }
-      if (resp.weather) {
-        var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
-        console.log(imgURL)
-        $("#weatherImg").attr("src", imgURL);
-        $("#weather-text").html(resp.weather[0].description);
-      }   
-    },
-    error: function(resp) {
-       alert("Error: " + e);
-       clearInterval(updateinter);
+    if (cels > 24) {
+      $("#temp-text").css("color", "red");
+    } else if (cels < 18) {
+      $("#temp-text").css("color", "blue");
     }
-  });
-}
-
-function getLocation() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      getWeather(position.coords.latitude, position.coords.longitude);
-    })
-  } else {
-    alert("geolocation not available" + e);
-    clearInterval(updateinter);
   }
-}
-var i = 0;
-var updateinter = setInterval(function(){
-  console.log("iteration# " + i++);
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      getWeather(position.coords.latitude, position.coords.longitude);
-    })
-  } else {
-    alert("geolocation not available" + e);
-  }
-}, 300000);
+  //handles response data and formats it accordingly since it is an asynchronous response object all data handling and formatting must be done within this function.
+  function dataHandler(data) {
 
-  
-getLocation();
+    formatTemperature(data.main.temp);
+
+    if (data.main.temp && data.name && data.sys) {
+      // display location name
+      $("#city-text").html(data.name + ", " + data.sys.country);
+      // display icon
+      if (data.weather) {
+        var imgURL = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        $("#weatherImg").attr("src", imgURL);
+        $("#weather-text").html(data.weather[0].description);
+      }
+      // display wind speed
+      if (data.wind) {
+        var knots = data.wind.speed * 1.9438445;
+        $windText.html(knots.toFixed(1) + " Knots");
+      }
+    }
+  }
+  //This calls the api with the correct coordinates provided by the getLocation function
+  function getWeather(lat, lon) {
+    var apiURI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=06170c100199dbae1e223cc3dfad960b";
+
+    return $.ajax({
+      url: apiURI,
+      dataType: "json",
+      type: "GET",
+      async: "true",
+    });
+  }
+
+  //Passes the browser's geolocation into the getWeather function once its done the response from the getWeather call will be passed to the data handler for formatting.
+  var counter = 0;
+
+  function getLocation() {
+    console.log("Update# " + counter++);
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        getWeather(position.coords.latitude, position.coords.longitude).done(dataHandler);
+      })
+    } else {
+      alert("geolocation not available");
+    }
+  }
+  var updateInterval = setInterval(getLocation(), 300000);
+});
