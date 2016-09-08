1d468efe4e5267aa98b524d6e859e46e26bdda9c
Gottschling <gottscbr@vertafore.com>
2016-09-08 09:56:38 -0400

    Added a new project that is in the works

---
+++ js/wikidiver.js
@@ -0,0 +1,22 @@
+var clicked = false;
+
+$("#submit").on("click", 
+    function Go(){
+      var search = $("#search").val();
+      var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&namespace=0&profile=fuzzy&format=json";
+      clicked = true;
+
+      return $.ajax({
+        url: apiURL,
+        type:"GET",
+        async: "true"  
+      }).done(dataHandler);
+
+      console.log(search);
+    });
+
+if (clicked){
+  function dataHandler(data){
+    console.log(data);
+  }
+}
