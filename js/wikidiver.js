e925a7c9987c546e6bdda8e0510f6f16cac5863e
Gottschling <gottscbr@vertafore.com>
2016-09-08 10:52:20 -0400

    Working out an issue with headers

--- js/wikidiver.js
+++ js/wikidiver.js
@@ -1,17 +1,17 @@
 var clicked = false;
 
-$("#submit").on("click", 
+$('#submit').on('click', 
     function Go(){
-      var search = $("#search").val();
-      var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=10&namespace=0&profile=fuzzy&format=json";
+      var search = $('#search').val();
+      var apiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + search + '&limit=10&namespace=0&profile=fuzzy&format=json';
       clicked = true;
 
       return $.ajax({
         url: apiURL,
-        type:"GET",
-        async:"true",
-        xhrFields:{
-          "Access-Control-Allow-Origin": *;
+        type:'GET',
+        async:'true',
+        headers:{
+          'Access-Control-Allow-Origin': '*';
         }  
       }).done(dataHandler);
 
