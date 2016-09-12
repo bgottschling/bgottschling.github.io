fb809603d790f885d83e134cdfd1e8d20625d723
Gottschling <gottscbr@vertafore.com>
2016-09-12 09:04:45 -0400

    Improving style and readability

--- js/wikidiver.js
+++ js/wikidiver.js
@@ -1,5 +1,5 @@
 $('#submit').on('click', 
-    function Go(){
+    function Go(key){
       var search = $('#search').val();
       var apiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + search + '&limit=10&namespace=0&profile=fuzzy&format=json';
 
@@ -14,6 +14,23 @@ $('#submit').on('click',
 
     });
 
+$(document).on('keypress', 
+    function Go(key){
+      if(key.which == 13){
+        var search = $('#search').val();
+        var apiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + search + '&limit=10&namespace=0&profile=fuzzy&format=json';
+
+        $('#content-area').html('');
+
+        $.ajax({
+          url: apiURL,
+          type:'GET',
+          dataType: 'jsonp',
+          async:'true',  
+        }).done(dataHandler);
+      }
+
+    });
 
 
   function dataHandler(data){
@@ -23,7 +40,7 @@ $('#submit').on('click',
     if (data){
       for (var i=0; i < 10; i++){
 
-        $('#content-area').append('<div class="results row"><a href=' + data[3][i] + '><div class="well"><h2>' + data[1][i] + '</h2><p>' + data[2][i] + '</p></div></a></div>');
+        $('#content-area').append('<div class="results row"><a href=' + data[3][i] + '><div class="text-center well"><h2>' + data[1][i] + '</h2><p>' + data[2][i] + '</p></div></a></div>');
 
      }
     }
