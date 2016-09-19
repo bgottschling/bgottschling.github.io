8aeba024e7bf35e4db3963eda4b649120ec75efc
Gottschling <gottscbr@vertafore.com>
2016-09-19 17:23:27 -0400

    Adding and testing JS logic for tweaker.

--- js/tweaker.js
+++ js/tweaker.js
@@ -1,35 +1,29 @@
-$(window).load(
-	console.log('loaded'),
-	addChannels());
+$(window).load(getStream());
 
+var channels = ['freecodecamp','ESL_SC2','frankieonpcin1080p'];
+
+function 
 
 function addChannels(chan){
 	console.log('addChannels executed');
-	var channels = ['freecodecamp','ESL_SC2','frankieonpcin1080p'];
-
-	if (chan){
-		channels.push(chan);
-	}
-
-	for (var i = 0, len = channels.length; i < len; i++){
-		console.log("channel call iteration");
-		getStream(channels[i]);
-		channels.shift();
-	}
 
 }
 
 
-function getStream(chan) {
+function getStream() {
+	channels.forEach(function(chan){
+
+		var apiURL = 'https://api.twitch.tv/kraken/streams/' + chan +'';
+		console.log(chan);
+		$.ajax({
+			type:'GET',
+			dataType:'jsonp',
+			URL: apiURL
+		}).done(buildViewer);
+
+	});
 
 
-	var apiURL = 'https://api.twitch.tv/kraken/streams/' + chan +'';
-	console.log(chan);
-	$.ajax({
-		type:'GET',
-		dataType:'json',
-		URL: apiURL
-	}).done(buildViewer);
 }
 
 function buildViewer(data) {
@@ -38,10 +32,10 @@ function buildViewer(data) {
 	var $sec2 = $('#sec2');
 	var suggested = 0;
 	if (suggested < 3){
-		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
+		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
 		suggested++;	
 	} else if (suggested > 3){
-		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
+		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
 	}
 
 }
