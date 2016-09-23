b9ba544aff78d7a608552ff1d345ccc2b9078973
Gottschling <gottscbr@vertafore.com>
2016-09-23 10:07:28 -0400

    Testing calls with the new client-id to make calls valid.

--- js/tweaker.js
+++ js/tweaker.js
@@ -28,14 +28,13 @@ function getStream() {
 
 function buildViewer(data) {
 	console.log("channel is" + data);
-	var $sec1 = $('#sec1');
-	var $sec2 = $('#sec2');
+
 	var suggested = 0;
-	if (suggested < 3){
+	/*if (suggested < 3){
 		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
 		suggested++;	
 	} else if (suggested > 3){
 		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
-	}
+	}*/
 
 }
