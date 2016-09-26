65d318a4073c4d962d11746db9647d687257ee47
Brandon Gottschling <Brandon Gottschling>
2016-09-26 15:16:54 -0400

    Trying to work out Twitch API calls

--- js/tweaker.js
+++ js/tweaker.js
@@ -1,17 +1,5 @@
 //$(window).load(getStream());
 
-function buildViewer(data) {
-	console.log("channel is" + data);
-
-	var suggested = 0;
-	/*if (suggested < 3){
-		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
-		suggested++;	
-	} else if (suggested > 3){
-		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
-	}*/
-
-}
 
 function getStream() {
 	var channels = ['freecodecamp','ESL_SC2','frankieonpcin1080p'];
@@ -34,4 +22,18 @@ function getStream() {
 
 getStream();
 
+function buildViewer(data) {
+
+    console.log("success");
+	console.log("channel is" + data);
+
+	var suggested = 0;
+	/*if (suggested < 3){
+		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
+		suggested++;	
+	} else if (suggested > 3){
+		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ data.channel.name +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ data.channel.name +'<p></div></a></div>');
+	}*/
+
+}
 
