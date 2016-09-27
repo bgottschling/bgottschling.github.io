5d9c24a281ce1be8dd204099aa80fae07d150689
Brandon Gottschling <Brandon Gottschling>
2016-09-27 18:33:13 -0400

    Altered method of retrieving data.

--- js/tweaker.js
+++ js/tweaker.js
@@ -1,24 +1,12 @@
-//$(window).load(getStream());
 
 var suggested = 0;
 function getStream() {
-	var channels = ['freecodecamp','ESL_SC2','frankieonpcin1080p',"brndng"];
+	var channels = ['freecodecamp','ESL_SC2',"brndng"];
 
 
 	
 	channels.forEach(function(chan){
 		buildViewer(chan);
-		//var apiURL = 'https://api.twitch.tv/kraken'; //+ chan;
-		/*console.log(apiURL);
-		$.ajax({
-			type:'GET',
-			dataType:'json',
-			URL:apiURL,
-			headers:{
-				'Client-ID':'fogk4a9ms544lema5ki24jvdbz8g4wz'
-			},
-			async:'false'
-		}).done(buildViewer);*/
 	});
 	
 }
@@ -32,7 +20,7 @@ function buildViewer(chanData) {
 
 	
 	if (suggested < 3){
-		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
+		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ chanData + '" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ chanData +'"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
 		suggested++;	
 	} else{
 		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
