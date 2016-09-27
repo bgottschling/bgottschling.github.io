25debcc9c35fdd1b8181b1c91017351ecb12e938
Brandon Gottschling <Brandon Gottschling>
2016-09-27 18:57:47 -0400

    Altered method of retrieving data.

--- js/tweaker.js
+++ js/tweaker.js
@@ -20,10 +20,10 @@ function buildViewer(chanData) {
 
 	
 	if (suggested < 3){
-		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="http://player.twitch.tv/?channel='+ chanData +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="http://player.twitch.tv/?channel='+ chanData +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
+		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="http://player.twitch.tv/?channel='+ chanData +'&autoplay=false" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="http://player.twitch.tv/?channel='+ chanData +'&autoplay=false"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
 		suggested++;	
 	} else{
-		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="http://player.twitch.tv/?channel='+ chanData +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="http://player.twitch.tv/?channel='+ chanData +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
+		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="http://player.twitch.tv/?channel='+ chanData +'&autoplay=false" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="http://player.twitch.tv/?channel='+ chanData +'&autoplay=false"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
 	}
 
 }
