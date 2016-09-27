603a9b4060f645975951a08a0f42f57c2e8a3e65
Brandon Gottschling <Brandon Gottschling>
2016-09-27 17:08:03 -0400

    Altered method of retrieving data.

--- js/tweaker.js
+++ js/tweaker.js
@@ -31,10 +31,10 @@ function buildViewer(chanData) {
 	console.log("channel is" + chanData);
 
 	var suggested = 0;
-	if (suggested < 3){
+	if (suggested < 2){
 		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
 		suggested++;	
-	} else if (suggested > 3){
+	} else if (suggested > 2){
 		$('#sec2').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
 	}
 
