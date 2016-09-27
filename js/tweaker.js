99f8640616b95e70340be17149fb95c28e691dc4
Brandon Gottschling <Brandon Gottschling>
2016-09-27 17:09:34 -0400

    Altered method of retrieving data.

--- js/tweaker.js
+++ js/tweaker.js
@@ -1,6 +1,6 @@
 //$(window).load(getStream());
 
-
+var suggested = 0;
 function getStream() {
 	var channels = ['freecodecamp','ESL_SC2','frankieonpcin1080p',"brndng"];
 
@@ -30,7 +30,7 @@ function buildViewer(chanData) {
     console.log("success");
 	console.log("channel is" + chanData);
 
-	var suggested = 0;
+	
 	if (suggested < 2){
 		$('#sec1').append('<div class="well col-sm-4 stream-item "><a href="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true" target="_blank" class="thumbnail"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://player.twitch.tv/?channel='+ chanData +'&data-paused=true"></iframe></div><div class="text-center caption"><p>'+ chanData +'<p></div></a></div>');
 		suggested++;	
